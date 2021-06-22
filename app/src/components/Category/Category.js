f0309193686a94b701121a12f0646b84b7db2189
Tin Win Hlaing <tinwinhlaing.ucsy@gmail.com>
2021-06-22 12:44:14 +0630

    Category design final

--- app/src/components/Category/Category.js
+++ app/src/components/Category/Category.js
@@ -6,77 +6,138 @@ import {
   SafeAreaView,
   Text,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import {
-  Button,
-  Paragraph,
-  Dialog,
-  Portal,
-  TextInput,
-  Provider
-} from "react-native-paper";
-import IonIcons from "react-native-vector-icons/AntDesign";
+import { Button, Card, TextInput, Provider, Title } from "react-native-paper";
+
+const topics = [
+  {
+    emoji: "🍻",
+    text: "Entertainment",
+  },
+  {
+    emoji: "🐈",
+    text: "Cats",
+  },
+  {
+    emoji: "🦾",
+    text: "Robots",
+  },
+  {
+    emoji: "🎉",
+    text: "Party",
+  },
+  {
+    emoji: "🌍",
+    text: "World",
+  },
+  {
+    emoji: "📚",
+    text: "Books",
+  },
+  {
+    emoji: "👘",
+    text: "Fashion",
+  },
+  {
+    emoji: "📱",
+    text: "Applications",
+  },
+  {
+    emoji: "📸",
+    text: "Photography",
+  },
+  {
+    emoji: "🧠",
+    text: "Ideas",
+  },
+  {
+    emoji: "⚔️",
+    text: "War",
+  },
+  {
+    emoji: "💼",
+    text: "Business",
+  },
+  {
+    emoji: "🎭",
+    text: "Theater",
+  },
+  {
+    emoji: "📮",
+    text: "Job",
+  },
+];
 
 const Category = () => {
-  const [visible, setVisible] = React.useState(false);
-
-  const showDialog = () => setVisible(true);
-
-  const hideDialog = () => setVisible(false);
+  const [category, setCategory] = React.useState("");
+  const [sticker, setSticker] = React.useState("");
 
   return (
-      <Provider>
-    <SafeAreaView
-      style={{ backgroundColor: "#fff", height: "100%", width: "100%" }}
-    >
-      <View style={category_style.header}>
-        <Image
-          style={category_style.logo}
-          source={require("../../assets/images/logo.png")}
-        />
-        <Text style={category_style.headerText}>
-          "Hey, let's create {"\n"} category & choose icon to use in your income
-          and expense."
-        </Text>
-      </View>
-      <TextInput
-        mode="outlined"
-        theme={{ colors: { primary: "#0D3858" }, roundness: 15 }}
-        label="Enter category title"
-        style={category_style.input}
-      />
-      <Text style={category_style.inputExp}>eg: food, drink, etc...</Text>
-      <View style={category_style.stickerCard}>
-        <TouchableOpacity>
-          <IonIcons
-            name="pluscircleo"
-            size={30}
-            style={category_style.addSticker}
-            onPress={showDialog}
-          />
-        <Text style={category_style.chooseStickerTxt}> Choose Sticker</Text>
-        </TouchableOpacity>
-        <Portal>
-          <Dialog visible={visible} onDismiss={hideDialog}>
-            <Dialog.Title>Choose Sticker</Dialog.Title>
-            <Dialog.Content>
-              <Paragraph>This is simple dialog</Paragraph>
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={hideDialog}>Done</Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </View>
-      <Button
-        mode="contained"
-        labelStyle={{ fontSize: 16, paddingTop: 7 }}
-        style={category_style.createBtn}
-        uppercase={false}
+    <Provider>
+      <SafeAreaView
+        style={{ backgroundColor: "#fff", height: "100%", width: "100%" }}
       >
-        Let's Create
-      </Button>
-    </SafeAreaView>
+        <View style={category_style.header}>
+          <Image
+            style={category_style.logo}
+            source={require("../../assets/images/logo.png")}
+          />
+          <Text style={category_style.headerText}>
+            "Hey, let's create {"\n"} category & choose icon to use in your
+            income and expense."
+          </Text>
+        </View>
+        <TextInput
+          mode="outlined"
+          theme={{ colors: { primary: "#0D3858" }, roundness: 15 }}
+          label="Enter category title"
+          style={category_style.input}
+          name="category"
+          value={category}
+          onChangeText={(category) => setCategory(category)}
+        />
+        <Text style={category_style.inputExp}>eg: food, drink, etc...</Text>
+        <View style={category_style.stickerCard}>
+          <View style={carousel_style.root}>
+            <Card style={carousel_style.cardStyle}>
+              <Title style={carousel_style.title}>Choose Sticker</Title>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+                {topics.map((category) => {
+                  return (
+                    <TouchableOpacity
+                      onPress={() => setSticker(category.emoji)}
+                    >
+                      <Card style={carousel_style.iconsCard}>
+                        <Text key={category.text} style={carousel_style.icons}>
+                          {category.emoji}
+                        </Text>
+                      </Card>
+                    </TouchableOpacity>
+                  );
+                })}
+              </ScrollView>
+            </Card>
+          </View>
+        </View>
+        <Text style={category_style.displayTxt}>
+          Your Categoy: {"  "}
+          <Text style={category_style.displayInnterTxt}>
+            {category}  {sticker}
+          </Text>
+        </Text>
+        <Button
+          mode="contained"
+          labelStyle={{ fontSize: 16, paddingTop: 7 }}
+          style={category_style.createBtn}
+          uppercase={false}
+        >
+          Let's Create
+        </Button>
+      </SafeAreaView>
     </Provider>
   );
 };
@@ -129,6 +190,16 @@ const category_style = StyleSheet.create({
   chooseStickerTxt: {
     alignSelf: "center",
   },
+  displayTxt: {
+    top: 100,
+    left: 60,
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#0d3858",
+  },
+  displayInnterTxt:{
+    color: "#000"
+  },  
   createBtn: {
     backgroundColor: "#467ca4",
     borderRadius: 20,
@@ -136,5 +207,33 @@ const category_style = StyleSheet.create({
     width: 150,
     top: 120,
     alignSelf: "center",
+  },
+});
+
+const carousel_style = StyleSheet.create({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title:{
+    fontSize: 13,
+    textAlign: "center",
+    marginBottom: 0,
+    color: "#fff",
+    fontWeight: "bold"
+  },
+  cardStyle: {
+    width: 320,
+    height: 90,
+    backgroundColor: "#0d3858",
+  },
+  iconsCard: {
+    margin: (0,8,8,8),
+  },
+  icons: {
+    padding: 5,
+    alignSelf: "center",
+    fontSize: 20,
   },
 });
